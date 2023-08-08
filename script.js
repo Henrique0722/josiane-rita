8ed3a1b6880a9c9fe8d77819c613e71b8dbabc02
Henrique0722 <108092109+Henrique0722@users.noreply.github.com>
2023-08-08 11:06:19 -0300

    menu mobile
    menu para telefone desenvolvido com sucesso

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ btn.addEventListener('click', function() {
         container.style.display = 'block'
     }
 })
+
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "imagens/menu.png";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "imagens/close.png";
+    }
+}
